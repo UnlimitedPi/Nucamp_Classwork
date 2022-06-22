7de53185406f4a317581ca8e4c301af4f4156a34
UnlimitedPi <ryan97castillo@outlook.com>
2022-06-22 13:48:28 -0500

    Added 'class User'

--- homework/test.py
+++ homework/test.py
@@ -1,6 +1,20 @@
 def function(name):
-    return f'First test function in GitHub by {name}!'
+    return f'First test function in GitHub by {name}!\n'
 
 hello = function('UnlimitedPi')
 
 print(hello)
+
+class User:
+    def __init__(self, f_name, l_name, age, job):
+        self.f_name = f_name
+        self.l_name = l_name
+        self.age = age
+        self.job =job
+    
+    def list_user(self):
+        return f'Your user info: {self.l_name}, {self.f_name} - Age: {self.age}, Job: {self.job}.'
+
+user_1 = User('Ryan', 'Castillo', '24', 'Software Dev')
+
+print(user_1.list_user())
